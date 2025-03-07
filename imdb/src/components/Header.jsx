07c35cc0d807b6559bbfcac779fa5c0208bb0307
Samir kc <samirkc6262@gmail.com>
2025-03-07 08:58:29 +0545

    About section updated

--- imdb/src/components/Header.jsx
+++ imdb/src/components/Header.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import MenuItems from "./MenuItems";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import Link from "next/link";
+import DarkModeSwitch from "./DarkModeSwitch";
 
 const Header = () => {
   return (
@@ -10,9 +12,14 @@ const Header = () => {
         <MenuItems title="home" address="/" Icon={AiFillHome} />
         <MenuItems title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <div className="flex items-center">
-        <span className="bg-amber-500 px-2 py-1 text-2xl rounded-lg">IMDb</span>
-        <span className="hidden sm:inline">Clone</span>
+      <div className="flex items-center gap-4">
+        <DarkModeSwitch />
+        <Link href="/" className="flex items-center">
+          <span className="bg-amber-500 px-2 py-1 text-2xl rounded-lg">
+            IMDb
+          </span>
+          <span className="text-xl hidden sm:inline">Clone</span>
+        </Link>
       </div>
     </div>
   );
--- imdb/src/components/MenuItems.jsx
+++ imdb/src/components/MenuItems.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const MenuItems = ({ title, address, Icon }) => {
   return (
     <div>
-      <Link href={address} className="flex items-center">
+      <Link href={address} className="hover:text-amber-500 flex items-center">
         <Icon className="text-2xl sm:hidden " />
         <p className="uppercase hidden sm:inline text-sm">{title}</p>
       </Link>
